e24033e258b97700b61007ec5a3b8edc29cb8cf9
justrynabedev <justintrynabedev@gmail.com>
2026-03-03 12:26:01 +0100

    03/03/2026 pt 3

--- mooncolors-shop/app.py
+++ mooncolors-shop/app.py
@@ -66,4 +66,5 @@ def obtener_portada():
         return jsonify({"error": "Fallo en la base de datos", "detalle": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
